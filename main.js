694487a81aa4e850b5b8a5f26eca6464717078e0
Jack <jack.70aw@gmail.com>
2025-04-25 15:21:28 +0330

    send price to channel every 1h

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
-require('dotenv').config(); // Load environment variables from a .env file
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
-// Your bot token from BotFather, stored in an environment variable
+dotenv.config();
+
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const channelId = process.env.CHANNEL_ID
+const apiUrl = `https://api.nobitex.ir/market/stats`;
 
-// API URL with the API key loaded from environment variables
-const apiUrl = `http://api.navasan.tech/latest/?api_key=${process.env.API_KEY}`;
-
-// Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
 
-// Function to fetch JSON from API
 async function getJsonFromApi() {
     try {
         const response = await fetch(apiUrl);
@@ -19,33 +17,45 @@ async function getJsonFromApi() {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-
         const data = await response.json();
-        return data; // Return the fetched data
+        return data;
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
         return null;
     }
 }
 
-// Listen for a message from the user
 bot.onText(/\/getdata/, async (msg) => {
     const chatId = msg.chat.id;
 
-    // Fetch the JSON data from the API
     const data = await getJsonFromApi();
 
-    // If data is found, send it to the user
     if (data) {
-        // If the data contains relevant information, format it nicely
         const formattedData = `
-        Data from API:
-        - Name: ${data.name || 'N/A'}
-        - Description: ${data.description || 'No description available'}
-        - Other info: ${JSON.stringify(data.otherInfo || 'No additional info')}`;
+        بیت کوین: ${data.stats["btc-usdt"].latest}
+        تتر: ${data.stats["usdt-rls"].latest}
+        `
+
 
         bot.sendMessage(chatId, formattedData);
     } else {
         bot.sendMessage(chatId, 'Sorry, there was an issue fetching the data.');
     }
 });
+
+async function sendToChannel() {
+    const data = await getJsonFromApi()
+
+    if (data) {
+        const formattedData = `
+        بیت کوین: ${data.stats["btc-usdt"].latest}
+        تتر: ${data.stats["usdt-rls"].latest}
+        `
+        bot.sendMessage(channelId, formattedData)
+    } else {
+        bot.sendMessage(channelId, "not working")
+    }
+}
+
+setInterval(sendToChannel, 3600000); // 1 hour = 3600000 ms
+sendToChannel(); 
